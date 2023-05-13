5e8851462b5c04a3da54338b5db738da245b37e1
nupurSgh <nupursgh24@gmail.com>
2023-05-13 10:34:15 +0530

    add view engine

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const homeController = require('../controllers/home_controllers');
 
 console.log('router loaded');
-router.get('/', homeController.home)
+router.get('/', homeController.home);
+router.use('/users', require('./users'));
 
 module.exports = router;
